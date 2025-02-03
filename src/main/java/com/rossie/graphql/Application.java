63c8b7a0e8989780399e3df6f06ebad2efbacd15
Marcel Poots <m.poots69@gmail.com>
2025-02-03 19:01:18 +0100

    initial commit

---
+++ src/main/java/com/rossie/graphql/Application.java
@@ -0,0 +1,29 @@
+package com.rossie.graphql;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    ApplicationRunner applicationRunner(BookRepository bookRepository) {
+        return args -> {
+
+            bookRepository.saveAll(List.of(
+                    new Book("LOTR", "Tolkien"),
+                    new Book("The Blood prince", "JK Rowlins"),
+                    new Book("The Old man and the sea", "Hemingway")
+            ));
+        };
+    }
+}
---
+++ src/main/java/com/rossie/graphql/Book.java
@@ -0,0 +1,24 @@
+package com.rossie.graphql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+}
